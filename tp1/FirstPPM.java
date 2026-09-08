d4eea93249fcb4c15b6ff60f0285e020f4d264f7
ANAELLE HINARD <anaelle.hinard@iut.local>
2026-09-08 14:55:56 +0200

    ajout des lignes de codes pour générer l'image

--- tp1/FirstPPM.java
+++ tp1/FirstPPM.java
@@ -7,11 +7,10 @@ public class FirstPPM {
             FileWriter writer = new FileWriter("FirstPPM.ppm");
 
             writer.write("P3\n");
-            // Écriture des dimensions
-            // Écriture de la valeur maximal
-            // Écriture des pixels
-            // Première ligne : rouge, vert, bleu
-            // Deuxième ligne : jaune, blanc, noir
+			writer.write("3 2\n");
+			writer.write("255\n");
+			writer.write("255 0 0 0 255 0 0 0 255\n");
+			writer.write("255 255 0 255 255 255 0 0 0\n");
 
             writer.close(); // Fermeture du fichier
 
