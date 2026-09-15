c995a9d82256634c925d6a35cfeb85fad1e391e9
ANAELLE HINARD <anaelle.hinard@iut.local>
2026-09-15 15:32:29 +0200

    image

--- tp1/Image.java
+++ tp1/Image.java
@@ -13,17 +13,17 @@ public class Image {
     /**
      * Constructeur : initialise une image vide.
      */
-    public Image(int width, int hauteur) {
+    public Image(int width, int height) {
         this.width = width;
         this.height = height;
-        pixels = new int[hauteur][largeur][3];
+        pixels = new int[width][height][3];
     }
 
     /**
      * Définit la couleur d'un pixel à la position (x, y)
      */
     public void setPixel(int x, int y, int r, int g, int b) {
-        if (x >= 0 && x < largeur && y >= 0 && y < hauteur) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
             pixels[y][x][0] = r;
             pixels[y][x][1] = g;
             pixels[y][x][2] = b;
@@ -34,6 +34,22 @@ public class Image {
      * Sauvegarde l'image au format texte PPM (P3)
      */
     public void save_txt(String filename) throws IOException {
-        // TODO : écrire le fichier PPM avec FileWriter
+        FileWriter writer = new FileWriter(filename);
+		writer.write("P3\n");
+		writer.write("3 2\n");
+		writer.write("255\n");
+		
+		for(int a = 0; a < height; a++){
+			for(int b = 0; b < width; b++){
+				
+				int c = pixels[a][b][0];
+				int d = pixels[a][b][1];
+				int e = pixels[a][b][2];
+				
+				writer.write(c + " " + d + " " + e + " ");
+			}
+			writer.write("\n");
+		}
+		
     }
 }
