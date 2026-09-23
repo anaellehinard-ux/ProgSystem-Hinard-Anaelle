f812502be335abf803ac62973e374569bbd9f5d0
anaellehinard-ux <anaelle.hinard@iut-rodez.fr>
2026-09-23 21:00:09 +0200

    Correction de la class Utils (bien endian)

--- tp2/Utils.java
+++ tp2/Utils.java
@@ -2,16 +2,16 @@ public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
 		
-		byte b1 = (byte) (value & 0xFF);
-		byte b2 = (byte) (value >> 8 & 0xFF);
-		byte b3 = (byte) (value >> 16 & 0xFF);
-		byte b4 = (byte) (value >> 24 & 0xFF);
+		byte b1 = (byte) ((value >> 24) & 0xFF);
+		byte b2 = (byte) ((value >> 16) & 0xFF);
+		byte b3 = (byte) ((value >> 8) & 0xFF);
+		byte b4 = (byte) (value & 0xFF);
 		
-		memory[offset] = b1;
+		memory[offset]     = b1;
 		memory[offset + 1] = b2;
 		memory[offset + 2] = b3;
 		memory[offset + 3] = b4;
-		
+    
 		return 4;
 	}
 
@@ -22,11 +22,11 @@ public class Utils {
         int b3 = memory[offset + 2] & 0xFF;
         int b4 = memory[offset + 3] & 0xFF;
 
-        return b1 | (b2 << 8) | (b3 << 16) | (b4 << 24);
+        return (b1 << 24) | (b2 << 16) | (b3 << 8) | b4;
     }
     public static int writeShort(byte[] memory, int offset, short value) {
-        memory[offset] = (byte) (value & 0xFF);
-        memory[offset + 1] = (byte) ((value >> 8) & 0xFF);
+        memory[offset] = (byte) ((value >> 8) & 0xFF);
+        memory[offset + 1] = (byte) (value & 0xFF);
         return 2;
     }
 
@@ -34,6 +34,6 @@ public class Utils {
         int b1 = memory[offset] & 0xFF;
         int b2 = memory[offset + 1] & 0xFF;
 
-        return (short) (b1 | (b2 << 8));
+        return (short) ((b1 << 8) | b2);
     }
 }
