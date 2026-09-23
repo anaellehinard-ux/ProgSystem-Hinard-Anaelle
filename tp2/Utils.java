7cac3446d1e5b3a5932ac72104f286556d3509ee
ANAELLE HINARD <anaelle.hinard@iut.local>
2026-09-23 17:23:39 +0200

    ajout de la classe readInt

--- tp2/Utils.java
+++ tp2/Utils.java
@@ -16,10 +16,14 @@ public class Utils {
 	}
 
     public static int readInt(byte[] memory, int offset) {
-        // TODO: Reconstituer le int sur 4 octets.
-        return 0;
-    }
+		
+        int b1 = memory[offset] & 0xFF;
+        int b2 = memory[offset + 1] & 0xFF;
+        int b3 = memory[offset + 2] & 0xFF;
+        int b4 = memory[offset + 3] & 0xFF;
 
+        return b1 | (b2 << 8) | (b3 << 16) | (b4 << 24);
+    }
     public static int writeShort(byte[] memory, int offset, short value) {
         // TODO: Écrire les 2 octets de 'value'.
         return 2;
